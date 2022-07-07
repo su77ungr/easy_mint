8100fd91043aa32cebc28fc45d8c5bb3c049b1d8
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-07 11:00:27 +0200

    Update create_offer.sh

--- create_offer.sh
+++ create_offer.sh
@@ -7,4 +7,6 @@ id_list2=${id_list::-1}
 sleep 3
 ./chia.exe wallet make_offer -f 2788499092 -o $id_list2:1 -r 9:0.959692898273 -p offers/$i.offer &&
 sleep 0.5
+sudo apt-get install qrencode -y && \
+raw_offer=$(sudo cat offers/$i.offer) | echo "$(tput setaf 2) $raw_offer" ; qrencode $raw_offer -t ANSI256
 done
