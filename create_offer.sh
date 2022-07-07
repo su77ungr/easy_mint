30fb53c092e2dd05d10403447c10ae8a59beec98
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-07 11:28:57 +0200

    Update create_offer.sh

--- create_offer.sh
+++ create_offer.sh
@@ -2,7 +2,15 @@
 for i in {20..400};
 do
 echo $i
+
 id_list=$(sed -n ${i}p NFT1_BRICKS/NFTID_list.txt)
+
+for i in {1..999}
+do
+echo $i
+./chia.exe wallet nft list -i 12 | grep "nft1" | get number $i >> NFT_ID_LIST.txt
+
+
 id_list2=${id_list::-1}
 sleep 3
 ./chia.exe wallet make_offer -f 2788499092 -o $id_list2:1 -r 9:0.959692898273 -p offers/$i.offer &&
