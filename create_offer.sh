cc3f3f91956030b4d4a0b519c093cb6ddb7b3acc
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-09 14:28:03 +0200

    Update create_offer.sh

--- create_offer.sh
+++ create_offer.sh
@@ -4,6 +4,18 @@ read -p "what's your Wallet ID? (i.e 5)" id
 ./chia.exe wallet nft list -f $finger -i $id | grep "nft1" >> NFT_ID_LIST.txt
 echo "your NFT IDs are stored in NFT_ID_LIST.txt"
 
+read -p "Do you want to run auto creation of the offers? (yes/no) " yn
+
+case $yn in 
+	yes ) echo ok, we will proceed;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo invalid response;
+		exit 1;;
+esac
+
+creating offers ...
+
 for i in {1..9}; do 
 ./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r 9:0.959692898273 -p offers/$i.offer
 done
