284feebd03ea070271c5842f4fa7c971879bd320
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-09 15:21:35 +0200

    Update create_bulk_offers.sh

--- create_bulk_offers.sh
+++ create_bulk_offers.sh
@@ -2,13 +2,9 @@
 read -p "what's your fingerprint (i.e 278xxxxxxxxx)" finger
 read -p "what's your Wallet ID for the NFTs? (i.e 5)" id
 read -p "what's your Wallet ID for the tokens in exchange? (i.e 1)" id2
-read -p "what's your NFT's royalty? (i.e 0.05 = 5% / 0.69 = 69%)" roy
-echo your royalty is $roy
 read -p "what's your NFT's royalty? (i.e 0.05 = 5% / 0.69 = 69%)" roy; neu=$(echo "$roy+1" | bc); neu2=$(echo "scale=3 ; 1 / $neu" | bc); echo $neu2
-
+echo your royalty is $roy - ergo your exchange rate to token is $neu2
 read -p "how many offers do you want to create?" num
-
-
 
 
 ./chia.exe wallet nft list -f $finger -i $id | grep "nft1" >> NFT_ID_LIST.txt
@@ -26,6 +22,7 @@ esac
 echo "$(tput setaf 2) CREATING OFFERS NOW"
 
 for i in $(seq 1 $num); do
-./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r $id2:$roy2>sleep 0.5
+./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r $id2:$roy2 -p offers/$i.offer &&
+sleep 0.5
 done
 echo "$(tput setaf 3)loop closed ... thank you for using my script. Follow me on the twiiter @chialisp"
