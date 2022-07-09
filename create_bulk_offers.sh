f72648b09bbe69ec069b4fe9f277408ede346b51
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-09 15:27:21 +0200

    Update create_bulk_offers.sh

--- create_bulk_offers.sh
+++ create_bulk_offers.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-read -p "what's your fingerprint (i.e 278xxxxxxxxx)" finger
-read -p "what's your Wallet ID for the NFTs? (i.e 5)" id
-read -p "what's your Wallet ID for the tokens in exchange? (i.e 1)" id2
-read -p "what's your NFT's royalty? (i.e 0.05 = 5% / 0.69 = 69%)" roy; neu=$(echo "$roy+1" | bc); neu2=$(echo "scale=3 ; 1 / $neu" | bc); echo $neu2
-echo your royalty is $roy - ergo your exchange rate to token is $neu2
+read -p "what's your fingerprint (i.e 278xxxxxxxxx) : " finger
+read -p "what's your Wallet ID for the NFTs? (i.e 5) : " id
+read -p "what's your Wallet ID for the tokens in exchange? (i.e 2) : " id2
+read -p "what's your NFT's royalty? (i.e 0.05 = 5%) : " roy; neu=$(echo "$roy+1" | bc); neu2=$(echo "scale=3 ; 1 / $neu" | bc)
+echo your royalty is $roy - ergo your exchange rate to token is $neu2 tokens
 read -p "how many offers do you want to create?" num
-
 
 ./chia.exe wallet nft list -f $finger -i $id | grep "nft1" >> NFT_ID_LIST.txt
 echo "your NFT IDs are stored in NFT_ID_LIST.txt"
 
-read -p "$(tput setaf 2) Do you want to run auto creation of the offers? (yes/no) " yn
+read -p "$(tput setaf 2)Do you want to continue with the auto creation of the offers? (yes/no) " yn
 case $yn in
         yes ) echo ok, we will proceed;;
         no ) echo exiting...;
