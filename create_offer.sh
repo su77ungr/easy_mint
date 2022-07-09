20ef4ad9161017e6b9be245cc49e45f2087b610f
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-09 14:28:35 +0200

    Update create_offer.sh

--- create_offer.sh
+++ create_offer.sh
@@ -17,5 +17,6 @@ esac
 creating offers ...
 
 for i in {1..9}; do 
-./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r 9:0.959692898273 -p offers/$i.offer
+./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r 9:0.959692898273 -p offers/$i.offer &&
+sleep 0.5
 done
