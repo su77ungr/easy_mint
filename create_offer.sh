9fb3c92c2615af070b442b0b676d0dd41b0af287
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-09 14:27:04 +0200

    Update create_offer.sh

--- create_offer.sh
+++ create_offer.sh
@@ -2,6 +2,7 @@
 read -p "what's your fingerprint (i.e 278xxxxxxxxx) - you can get it by ./chia.exe wallet show?" finger
 read -p "what's your Wallet ID? (i.e 5)" id
 ./chia.exe wallet nft list -f $finger -i $id | grep "nft1" >> NFT_ID_LIST.txt
+echo "your NFT IDs are stored in NFT_ID_LIST.txt"
 
 for i in {1..9}; do 
 ./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r 9:0.959692898273 -p offers/$i.offer
