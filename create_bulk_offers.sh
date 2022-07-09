600528c0ea958920e7c2b17e9891769de05e6b28
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-09 14:42:36 +0200

    Update create_bulk_offers.sh

--- create_bulk_offers.sh
+++ create_bulk_offers.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 read -p "what's your fingerprint (i.e 278xxxxxxxxx)" finger
 read -p "what's your Wallet ID? (i.e 5)" id
+read -p "how many offers do you want to create?" num
 ./chia.exe wallet nft list -f $finger -i $id | grep "nft1" >> NFT_ID_LIST.txt
 echo "your NFT IDs are stored in NFT_ID_LIST.txt"
 
-read -p "Do you want to run auto creation of the offers? (yes/no) " yn
+read -p "$(tput setaf 2) Do you want to run auto creation of the offers? (yes/no) " yn
 
-case $yn in 
-	yes ) echo ok, we will proceed;;
-	no ) echo exiting...;
-		exit;;
-	* ) echo invalid response;
-		exit 1;;
+case $yn in
+        yes ) echo ok, we will proceed;;
+        no ) echo exiting...;
+                exit;;
+        * ) echo invalid response;
+                exit 1;;
 esac
 
 echo "$(tput setaf 2) CREATING OFFERS NOW"
 
-for i in {1..9}; do 
-./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r 9:0.959692898273 -p offers/$i.offer &&
-sleep 0.5
+for i in $(seq 1 $num); do
+./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r 9:0.95969289>sleep 0.5
 done
+echo "$(tput setaf 3)loop closed ... thank you for using my script. Follow me on the twiiter @chialisp"
