c9589a9529252c49a8105057841094b728a05a7b
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-09 15:37:13 +0200

    Update create_bulk_offers.sh

--- create_bulk_offers.sh
+++ create_bulk_offers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-read -p "what's your fingerprint (i.e 278xxxxxxxxx) : " finger
-read -p "what's your Wallet ID for the NFTs? (i.e 5) : " id
-read -p "what's your Wallet ID for the tokens in exchange? (i.e 2) : " id2
-read -p "what's your NFT's royalty? (i.e 0.05 = 5%) : " roy; neu=$(echo "$roy+1" | bc); neu2=$(echo "scale=3 ; 1 / $neu" | bc)
+read -p "Enter your fingerprint (i.e 278xxxxxxxxx) :" finger
+read -p "Wallet ID for the NFTs? (i.e 5) :" id
+read -p "Wallet ID for the token you request? (i.e 2) :" id2
+read -p "NFT's royalty? (i.e 0.05 = 5%) :" roy; neu=$(echo "$roy+1" | bc); neu2=$(echo "scale=3 ; 1 / $neu" | bc)
 echo your royalty is $roy - ergo your exchange rate to token is $neu2 tokens
-read -p "how many offers do you want to create?" num
+read -p "how many offers do you want to create? :" num
 echo ""
 echo "creating NFT ID list now ..."
 
@@ -13,14 +13,12 @@ echo "your NFT IDs are stored in NFT_ID_LIST.txt"
 
 read -p "$(tput setaf 2)Do you want to continue with the auto creation of the offers? (yes/no) " yn
 case $yn in
-        yes ) echo ok, we will proceed;;
+        yes ) proceed...;;
         no ) echo exiting...;
                 exit;;
         * ) echo invalid response;
                 exit 1;;
 esac
-
-echo "$(tput setaf 2) CREATING OFFERS NOW"
 
 for i in $(seq 1 $num); do
 ./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r $id2:$neu2 -p offers/$i.offer &&
