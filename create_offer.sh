e23c80b8ce8b5d6c87b19b6f73dbf61098379a2f
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-09 14:26:28 +0200

    Update create_offer.sh

--- create_offer.sh
+++ create_offer.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-for i in {20..400};
-do
-echo $i
+read -p "what's your fingerprint (i.e 278xxxxxxxxx) - you can get it by ./chia.exe wallet show?" finger
+read -p "what's your Wallet ID? (i.e 5)" id
+./chia.exe wallet nft list -f $finger -i $id | grep "nft1" >> NFT_ID_LIST.txt
 
-id_list=$(sed -n ${i}p NFT1_BRICKS/NFTID_list.txt)
-
-for i in {1..999}
-do
-echo $i
-./chia.exe wallet nft list -i 12 | grep "nft1" | get number $i >> NFT_ID_LIST.txt
-
-
-id_list2=${id_list::-1}
-sleep 3
-./chia.exe wallet make_offer -f 2788499092 -o $id_list2:1 -r 9:0.959692898273 -p offers/$i.offer &&
-sleep 0.5
-sudo apt-get install qrencode -y && \
-raw_offer=$(sudo cat offers/$i.offer) && echo "$(tput setaf 2) $raw_offer" && qrencode $raw_offer -t ANSI256 | qrencode $raw_offer -t SVG > backup_qr$i.svg | echo "saved backup QR in backup_qr$i.svg" 
+for i in {1..9}; do 
+./chia.exe wallet make_offer -f $finger -o $(sed -n ${i}p NFT_ID_LIST.txt | tail -c -64 | cut -c -62):1 -r 9:0.959692898273 -p offers/$i.offer
 done
