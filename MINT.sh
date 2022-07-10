1410a13fae91dae0b1d571a9f49c69435c519443
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-10 18:02:48 +0200

    Update MINT.sh

--- MINT.sh
+++ MINT.sh
@@ -1,48 +1,62 @@
 #!/bin/bash
-
-# Image files (images/) and Metadata files (metadata/) should be directories inside easy_mint/ 
-
-# URI storage link (i.e with IPFS and your CID)
-URI=https://gateway.pinata.cloud/ipfs/QmVxrncdNzefaZQAmvNsP3ovpQCvPrAzeuoVqDYFDEEeph 
-# URI local storage link (directory your files are stored in)
 FOLDER_NAME_URI=images                                                              
-# MURI storage link (i.e GITHUB repo)
-MURI=https://raw.githubusercontent.com/bricksofchia/NFT1_metadata/main              
-# URI local storage link (directory your files are stored in)
 FOLDER_NAME_MURI=metadata                                                            
-# Number of NFTs to be minted
 NUM=9                                                                               
-# Royalties (420 = 4.20%)
 ROYALTY=420                                                                         
-# Wallet's fingerprint
 FINGERPRINT=278XXXXXXX 
-# Wallet's ID 
 WALLET_ID=12
-# Receive Address (royalties)
 ROYALTY_ADDRESS=txch1srfpkw8rfcxzt37the4xtunld5en4ac5mp92duxerlpf2t4u9rhqyv4pfq   
-# Receive address (NFTs)
-RECEIVE_ADDRESS=txch1srfpkw8rfcxzt37the4xtunld5en4ac5mp92duxerlpf2t4u9rhqyv4pfq      
+RECEIVE_ADDRESS=txch1srfpkw8rfcxzt37the4xtunld5en4ac5mp92duxerlpf2t4u9rhqyv4pfq  
+API_KEY="{YOUR_API_KEY_FOR_NFT_STORAGE}"
 
 for i in $(seq 1 $NUM); do
+fname="./$FOLDER_NAME_URI/$i.png"
+
+response=`curl -s -X 'POST' "https://api.nft.storage/upload" \
+  -H "accept: application/json" \
+  -H "Content-Type: image/*" \
+  -H "Authorization: Bearer $API_KEY" \
+  --data-binary "@$fname"`
+
+cid=`echo $response | jq -r '.value.cid'`
+URI="https://${cid}.ipfs.nftstorage.link" &&
+echo $URI
+
 sample1=$(sha256sum -b $FOLDER_NAME_URI/$i.png | cut -c -64) &&
-sample2=$(curl -s $URI/$i.png | sha256sum | cut -c -64) &&
+sample2=$(curl -s https://${cid}.ipfs.nftstorage.link | sha256sum | cut -c -64) &&
 if [ $sample1 == $sample2 ]
 then
 echo $(tput setaf 2) hashes match $(tput setaf 5) NFT Nr. $i ${sample1:0:5}...  ${sample2:0:5}...
 echo $sample1 >> hashtable_URI.txt
+echo $URI >> table_URI.txt
 else
 echo $(tput setaf 1) DISMATCH at $i! $test2 --- $test3 >> hashtable_URI.log
 fi
 done
 echo $(tput setaf 7) "hashtable_URI.txt created successfully"
 
+# same for emtadata 
+
 for i in $(seq 1 $NUM); do
+fname="./$FOLDER_NAME_MURI/metadata$i.json"
+
+response=`curl -s -X 'POST' "https://api.nft.storage/upload" \
+  -H "accept: application/json" \
+  -H "Content-Type: image/*" \
+  -H "Authorization: Bearer $API_KEY" \
+  --data-binary "@$fname"`
+
+cid=`echo $response | jq -r '.value.cid'`
+URI="https://${cid}.ipfs.nftstorage.link" &&
+echo $URI
+
 sample1=$(sha256sum -b $FOLDER_NAME_MURI/metadata$i.json | cut -c -64) &&
-sample2=$(curl -s $MURI/metadata$i.json | sha256sum | cut -c -64) &&
+sample2=$(curl -s https://${cid}.ipfs.nftstorage.link | sha256sum | cut -c -64) &&
 if [ $sample1 == $sample2 ]
 then
 echo $(tput setaf 2) hashes match $(tput setaf 5) NFT Nr. $i ${sample1:0:5}...  ${sample2:0:5}...
 echo $sample1 >> hashtable_MURI.txt
+echo $URI >> table_MURI.txt
 else
 echo $(tput setaf 1) DISMATCH at $i! $test2 --- $test3 >> hashtable_MURI.log
 fi
@@ -60,9 +74,12 @@ case $yn in
 esac 
 
 for i in $(seq 1 $NUM); do
+URI=$(sed -n ${i}p table_URI.txt)
 URI_HASH=$(sed -n ${i}p hashtable_URI.txt)
+MURI=$(sed -n ${i}p table_MURI.txt)
 MURI_HASH=$(sed -n ${i}p hashtable_MURI.txt)
+
 echo $(tput setaf 7) "MINTING $i ..."
-cd .. && ./chia.exe wallet nft mint -f $FINGERPRINT -i $WALLET_ID -ra $ROYALTY_ADDRESS -ta $RECEIVE_ADDRESS -u $URI/$i.png -nh $URI_HASH -mu  $MURI/metadata$i.json -mh $MURI_HASH -rp $ROYALTY -m 0.000615 && cd easy_mint/
+cd .. && ./chia.exe wallet nft mint -f $FINGERPRINT -i $WALLET_ID -ra $ROYALTY_ADDRESS -ta $RECEIVE_ADDRESS -u $URI -nh $URI_HASH -mu  $MURI -mh $MURI_HASH -rp $ROYALTY -m 0.000615 && cd easy_mint/
 sleep 53
 done
