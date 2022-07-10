7ce29cd728fb59b698af5173ccee4e93acdb89a6
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-09 20:25:55 -0400

    Add files via upload

---
+++ minting_script/bulk_mint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+for z in {1..999}
+do
+echo $i
+sleep 1
+uri_default=(https://ipfs.io/ipfs/{YOUR_CID}/$i.png)
+echo uri url: $uri_default
+uri_hash=$(sed -n ${i}p hashtable_URI.txt)
+echo uri hash: $uri_hash
+metadata_default=(https://raw.githubusercontent.com/bricksofchia/NFT1_metadata/main/metadata$i.json)
+echo metadata url:$metadata_default
+metadata_hash=$(sed -n ${i}p hashtable_MURI.txt)
+echo metadata hash: $metadata_hash
+echo "minting now ..."
+sleep 2
+./chia.exe wallet nft mint -f YOUR_FINGERPRINT -i 12 -ra YOUR_ADDRESS -ta YOUR_ADDRESS -u $uri_default -nh $uri_hash -mu $metadata_default -mh $metadata_hash -sn $i -st 999 -rp 420 -m 0.000615 &&
+sleep 53
+echo "mint ok"
+done
