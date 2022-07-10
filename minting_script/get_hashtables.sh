58fbf1803b3d40c6769e9f450e894a8cae387f1c
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-10 14:08:50 +0200

    Update get_hashtables.sh

--- minting_script/get_hashtables.sh
+++ minting_script/get_hashtables.sh
@@ -2,16 +2,20 @@
 
 # URI storage link (i.e): https://gateway.pinata.cloud/ipfs/QmVxrncdNzefaZQAmvNsP3ovpQCvPrAzeuoVqDYFDEEeph/69.png | you have to enter:
 URI=https://gateway.pinata.cloud/ipfs/QmVxrncdNzefaZQAmvNsP3ovpQCvPrAzeuoVqDYFDEEeph
+# URI local storage link (i.e): /images
+FOLDER_NAME_URI=images
 
 # MURI storage link (i.e): https://raw.githubusercontent.com/bricksofchia/NFT1_metadata/main/metadata69.json | you have to enter: 
 MURI=https://raw.githubusercontent.com/bricksofchia/NFT1_metadata/main
+# URI local storage link (i.e): /images
+FOLDER_NAME_MURI=metadata
 
 # Number of NFTs 
 NUM=999
 
 for i in $(seq 1 $NUM);
 do
-sample1=$(sha256sum -b images/$i.png | cut -c -64) &&
+sample1=$(sha256sum -b $FOLDER_NAME_URI/$i.png | cut -c -64) &&
 sample2=$(curl -s $URI/$i.png | sha256sum | cut -c -64) &&
 if [ $sample1 == $sample2 ]
 then
@@ -23,11 +27,11 @@ fi
 done
 echo "hashtable_URI.txt created successfully"
 
-# now same procedure for the metadata 
+# same procedure for the metadata 
 
 for i in $(seq 1 $NUM);
 do
-sample1=$(sha256sum -b metadata/metadata$i.json | cut -c -64) &&
+sample1=$(sha256sum -b $FOLDER_NAME_MURI/metadata$i.json | cut -c -64) &&
 sample2=$(curl -s $MURI/metadata$i.json | sha256sum | cut -c -64) &&
 if [ $sample1 == $sample2 ]
 then
