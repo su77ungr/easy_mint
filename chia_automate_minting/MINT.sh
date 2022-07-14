5daf7a5817e26da10aee3355c683c3689feb6b63
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-14 18:34:45 +0200

    Update MINT.sh

--- chia_automate_minting/MINT.sh
+++ chia_automate_minting/MINT.sh
@@ -2,46 +2,53 @@
 ###        INPUT YOUR CREDENTIALS        ### 
 ############################################
 
-FILE_TYPE_OF_IMAGES="png" #could be jpg, jpeg too example
-#named 1.PNG, 2.PNG ... 1377.PNG and stored inside images/ example
+#EXAMPLE could be jpg, jpeg too 
+IMAGE_SUFFIX="png"
+#EXAMPLE 1.png, 2.png ... eave it as it is / else enter "friend" for friend1.png, friend2.png ...
+IMAGE_PREFIX="" 
+#EXAMPLE
+METADATA_SUFFIX="json"
+#EXAMPLE for 1.json, 2.json ... leave it as it is / else enter "friend" for friend1.json, friend2.json ...
+METADATA_PREFIX="metadata" 
+#EXAMPLE stored inside images/ 
 FOLDER_NAME_URI="images"    
-#named 1.JSON, 2.JSON ... 1377.JSON and stored inside metadata/ example
+#EXAMPLE stored inside metadata/ 
 FOLDER_NAME_MURI="metadata"
-#Number of NFTs to mint example
+#EXAMPLE Number of NFTs to mint 
 NUM=9     
-#Royalty (420=4.20%) example
+#EXAMPLE Royalty (420=4.20%) 
 ROYALTY=420  
-#example
+#EXAMPLE example
 FINGERPRINT="1518460169"
-#your nft wallet's ID
+#EXAMPLE your nft wallet's ID
 WALLET_ID=12
-#receive address for the royalties example
+#EXAMPLE receive address for the royalties 
 ROYALTY_ADDRESS="txch1zgyhkjf90ducz0y35fsxsx48cc0k6zjzcpyj5zj0g0v8una6f9us6xg62l"   
-#receive address which the NFTs get sent to after minting example
+#EXAMPLE receive address which the NFTs get sent to after minting 
 RECEIVE_ADDRESS="txch1zgyhkjf90ducz0y35fsxsx48cc0k6zjzcpyj5zj0g0v8una6f9us6xg62l"
-#fee for every mint (0.0000615 XCH per NFT) example
+#EXAMPLE fee for every mint (0.0000615 XCH per NFT) 
 FEE=0.000615 
-#example
-API_KEY="<enterhere>"
+#EXAMPLE api_key for nft.storage
+API_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDE3N2VGNTMwMWQzZTQ0Mzg0MzhCNjNFMmVlRDI0OTI2QkU3ZDA3MjgiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1NzQ2NTgwNjA1NSwibmFtZSI6Im1pbnQifQ.Cmc8U82mDx5A7y6dQyEabDk13d7IDKYYdrZnA5A2kQk"
 
 ###  DO NOT TOUCH SCRIPT AFTER THIS LINE ### 
 ############################################
 
-for i in $(seq 1 $NUM); do
+for i in $(seq 2 $NUM); do
 #uploads images onto nft.storage
-response1=$(curl -s -X POST https://api.nft.storage/upload -H "accept: application/json" -H "Content-Type: image/*" -H "Authorization: Bearer $API_KEY" --data-binary "@./$FOLDER_NAME_URI/$i.$FILE_TYPE_OF_IMAGES" --stderr -);
+response1=$(curl -s -X POST https://api.nft.storage/upload -H "accept: application/json" -H "Content-Type: image/*" -H "Authorization: Bearer $API_KEY" --data-binary "@./$FOLDER_NAME_URI/$IMAGE_NAME$i.$IMAGE_SUFFIX" --stderr -);
 cid1=`echo $response1 | jq -r '.value.cid'`
 URI1="https://${cid1}.ipfs.nftstorage.link" &&
 
 #uploads metadata onto nft.storage
-response2=$(curl -s -X POST https://api.nft.storage/upload -H "accept: application/json" -H "Content-Type: image/*" -H "Authorization: Bearer $API_KEY" --data-binary "@./$FOLDER_NAME_MURI/$i.json" --stderr -);
+response2=$(curl -s -X POST https://api.nft.storage/upload -H "accept: application/json" -H "Content-Type: image/*" -H "Authorization: Bearer $API_KEY" --data-binary "@./$FOLDER_NAME_MURI/$METADATA_PREFIX$i.$METADATA_SUFFIX" --stderr -);
 cid2=`echo $response2 | jq -r '.value.cid'`
 URI2="https://${cid2}.ipfs.nftstorage.link" &&
 
 #hashes files (locally stored / online stored)
-sample1_URI=$(sha256sum -b $FOLDER_NAME_URI/$i.$FILE_TYPE_OF_IMAGES | cut -c -64) &&
+sample1_URI=$(sha256sum -b $FOLDER_NAME_URI/$IMAGE_NAME$i.$IMAGE_SUFFIX | cut -c -64) &&
 sample2_URI=$(curl -s https://${cid1}.ipfs.nftstorage.link | sha256sum | cut -c -64) &&
-sample1_MURI=$(sha256sum -b $FOLDER_NAME_MURI/$i.json | cut -c -64) &&
+sample1_MURI=$(sha256sum -b $FOLDER_NAME_MURI/$METADATA_PREFIX$i.$METADATA_SUFFIX | cut -c -64) &&
 sample2_MURI=$(curl -s https://${cid2}.ipfs.nftstorage.link | sha256sum | cut -c -64) &&
 
 #compares hashes
@@ -49,7 +56,7 @@ if [ $sample1_URI == $sample2_URI ] && [ $sample1_MURI == $sample2_MURI ]
 then
 echo $sample1_URI >> hashtable_URI.txt; echo $URI1 >> table_URI.txt
 echo $sample1_MURI >> hashtable_MURI.txt; echo $URI2 >> table_MURI.txt
-echo -e "NFT DATA Nr. $i\n"
+echo -e "NFT DATA Nr. $i"
 echo $(tput setaf 2)HASHES MATCH$(tput setaf 7) URI : ${sample1_URI:0:7} --- ${sample2_URI:0:7}  --- ${URI1}
 echo $(tput setaf 2)HASHES MATCH$(tput setaf 7) MURI: ${sample1_MURI:0:7} --- ${sample2_MURI:0:7} --- ${URI2}
 else
@@ -63,7 +70,7 @@ case $yn in
         * ) echo exiting...; exit 1;;
 esac 
 #minting to the blockchain 
-for i in $(seq 1 $NUM); do echo $(tput setaf 7)"MINTING $i ...";
+for i in $(seq 2 $NUM); do echo $(tput setaf 7)"MINTING $i ...";
 echo "RUNNING: chia wallet nft mint -f $FINGERPRINT -i $WALLET_ID -ra $ROYALTY_ADDRESS -ta $RECEIVE_ADDRESS -u $(sed -n ${i}p table_URI.txt) -nh $(sed -n ${i}p hashtable_URI.txt) -mu $(sed -n ${i}p table_MURI.txt) -mh $(sed -n ${i}p hashtable_MURI.txt) -rp $ROYALTY -m $FEE"
 echo "IF SOMETHING SEEMS WRONG; HIT CTRL+C"; sleep 5
 chia wallet nft mint -f $FINGERPRINT -i $WALLET_ID -ra $ROYALTY_ADDRESS -ta $RECEIVE_ADDRESS -u $(sed -n ${i}p table_URI.txt) -nh $(sed -n ${i}p hashtable_URI.txt) -mu $(sed -n ${i}p table_MURI.txt) -mh $(sed -n ${i}p hashtable_MURI.txt) -rp $ROYALTY -m $FEE &&
