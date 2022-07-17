d3e63b0ece2f8776c76c296b1d20327a324d80f4
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-07-17 13:12:33 +0200

    removing file name variables

--- chia_automate_minting/MINT.sh
+++ chia_automate_minting/MINT.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
+ 
 ###        INPUT YOUR CREDENTIALS        ### 
 ############################################
-
 #EXAMPLE could be jpg, jpeg too 
 IMAGE_SUFFIX="png"
-#EXAMPLE 1.png, 2.png ... eave it as it is / else enter "friend" for friend1.png, friend2.png ...
-IMAGE_PREFIX="" 
 #EXAMPLE
-METADATA_SUFFIX="json"
-#EXAMPLE for 1.json, 2.json ... leave it as it is / else enter "friend" for friend1.json, friend2.json ...
-METADATA_PREFIX="metadata" 
-#EXAMPLE stored inside images/ 
 FOLDER_NAME_URI="images"    
 #EXAMPLE stored inside metadata/ 
 FOLDER_NAME_MURI="metadata"
@@ -36,19 +30,19 @@ API_KEY="eyJhbG......."
 
 for i in $(seq 1 $NUM); do
 #uploads images onto nft.storage
-response1=$(curl -s -X POST https://api.nft.storage/upload -H "accept: application/json" -H "Content-Type: image/*" -H "Authorization: Bearer $API_KEY" --data-binary "@./$FOLDER_NAME_URI/$IMAGE_PREFIX$i.$IMAGE_SUFFIX" --stderr -);
+response1=$(curl -s -X POST https://api.nft.storage/upload -H "accept: application/json" -H "Content-Type: image/*" -H "Authorization: Bearer $API_KEY" --data-binary "@./$FOLDER_NAME_URI/$i.$FILE_TYPE_OF_IMAGES" --stderr -) &&
 cid1=`echo $response1 | jq -r '.value.cid'`
 URI1="https://${cid1}.ipfs.nftstorage.link" &&
 
 #uploads metadata onto nft.storage
-response2=$(curl -s -X POST https://api.nft.storage/upload -H "accept: application/json" -H "Content-Type: image/*" -H "Authorization: Bearer $API_KEY" --data-binary "@./$FOLDER_NAME_MURI/$METADATA_PREFIX$i.$METADATA_SUFFIX" --stderr -);
+response2=$(curl -s -X POST https://api.nft.storage/upload -H "accept: application/json" -H "Content-Type: image/*" -H "Authorization: Bearer $API_KEY" --data-binary "@./$FOLDER_NAME_MURI/$i.json" --stderr -) &&
 cid2=`echo $response2 | jq -r '.value.cid'`
 URI2="https://${cid2}.ipfs.nftstorage.link" &&
 
 #hashes files (locally stored / online stored)
-sample1_URI=$(sha256sum -b $FOLDER_NAME_URI/$IMAGE_PREFIX$i.$IMAGE_SUFFIX | cut -c -64) &&
+sample1_URI=$(sha256sum -b $FOLDER_NAME_URI/$i.$FILE_TYPE_OF_IMAGES | cut -c -64) &&
 sample2_URI=$(curl -s https://${cid1}.ipfs.nftstorage.link | sha256sum | cut -c -64) &&
-sample1_MURI=$(sha256sum -b $FOLDER_NAME_MURI/$METADATA_PREFIX$i.$METADATA_SUFFIX | cut -c -64) &&
+sample1_MURI=$(sha256sum -b $FOLDER_NAME_MURI/$i.json | cut -c -64) &&
 sample2_MURI=$(curl -s https://${cid2}.ipfs.nftstorage.link | sha256sum | cut -c -64) &&
 
 #compares hashes
@@ -56,7 +50,7 @@ if [ $sample1_URI == $sample2_URI ] && [ $sample1_MURI == $sample2_MURI ]
 then
 echo $sample1_URI >> hashtable_URI.txt; echo $URI1 >> table_URI.txt
 echo $sample1_MURI >> hashtable_MURI.txt; echo $URI2 >> table_MURI.txt
-echo -e "NFT DATA Nr. $i"
+echo -e "NFT DATA Nr. $i\n"
 echo $(tput setaf 2)HASHES MATCH$(tput setaf 7) URI : ${sample1_URI:0:7} --- ${sample2_URI:0:7}  --- ${URI1}
 echo $(tput setaf 2)HASHES MATCH$(tput setaf 7) MURI: ${sample1_MURI:0:7} --- ${sample2_MURI:0:7} --- ${URI2}
 else
